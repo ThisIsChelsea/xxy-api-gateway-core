fd8238bac87b91a1529d73e0a39ad097802dbda8
xuxinyi <18752656953@163.com>
2023-07-04 10:06:30 +0800

    xxy-api-gateway-core-00

---
+++ api-gateway-core-00/src/main/java/com/xxy/gateway/Application.java
@@ -0,0 +1,10 @@
+package com.xxy.gateway;
+
+/**
+ * @Author xuxinyi
+ * @create 2023/7/4 10:04
+ * @Description
+ */
+public class Application {
+
+}
---
+++ api-gateway-core-00/src/test/java/com/xxy/gateway/ApiTest.java
@@ -0,0 +1,15 @@
+package com.xxy.gateway;
+
+import org.junit.Test;
+
+/**
+ * @Author xuxinyi
+ * @create 2023/7/4 10:04
+ * @Description
+ */
+public class ApiTest {
+    @Test
+    public void test_rpc(){
+
+    }
+}
